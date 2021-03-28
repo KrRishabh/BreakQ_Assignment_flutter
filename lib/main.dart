ff7186af14e581daac407eba682c1a6181d92918
KrRishabh <kr.rishabh72@gmail.com>
2021-03-29 01:31:11 +0530

    Initial commit on dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  return runApp(BreakQ());
+}
+
+class BreakQ extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BreakQ',
+      // theme: ThemeData.dark(),
+      // initialRoute: OpeningScreen.id,
+      // routes: {
+      //   OpeningScreen.id: (context) => OpeningScreen(),
+      //   Distance360Screen.id: (context) => Distance360Screen(),
+      //   GameToGameScreen.id: (context) => GameToGameScreen(),
+      // },
+    );
+  }
+}
